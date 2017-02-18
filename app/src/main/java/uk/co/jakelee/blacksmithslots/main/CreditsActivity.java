0ee7026a5ae6cb928a7275ee1c362fb98a7b64bb
Jake <bitbucket@jakelee.co.uk>
2017-02-18 06:41:38 +0000

    Placeholder credits

---
+++ app/src/main/java/uk/co/jakelee/blacksmithslots/main/CreditsActivity.java
@@ -0,0 +1,19 @@
+package uk.co.jakelee.blacksmithslots.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.jakelee.blacksmithslots.R;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+
+        // Android Wheel by ???
+        // Sugar ORM
+        //
+    }
+}
