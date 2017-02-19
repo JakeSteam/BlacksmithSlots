b9f2be38c8b9ba3c1e648b36bcd2731e655777b4
Jake <bitbucket@jakelee.co.uk>
2017-02-19 00:00:30 +0000

    Minor renames

--- app/src/main/java/uk/co/jakelee/blacksmithslots/helper/Constants.java
+++ app/src/main/java/uk/co/jakelee/blacksmithslots/helper/Constants.java
@@ -2,18 +2,18 @@ package uk.co.jakelee.blacksmithslots.helper;
 
 public class Constants {
     // Items
-    public final static int BRONZE_ORE = 1;
-    public final static int BRONZE_BAR = 2;
-    public final static int BRONZE_DAGGER = 3;
-    public final static int BRONZE_SWORD = 4;
+    public final static int RES_BRONZE_ORE = 1;
+    public final static int RES_BRONZE_BAR = 2;
+    public final static int RES_BRONZE_DAGGER = 3;
+    public final static int RES_BRONZE_SWORD = 4;
 
     // Slots
-    public final static int BRONZE_CRAFTING = 1;
-    public final static int BRONZE_WEAPON = 2;
-    public final static int BRONZE_ARMOUR = 3;
-    public final static int BRONZE_TOOL = 4;
-    public final static int BRONZE_ACCESSORY = 5;
-    public final static int BRONZE_MISC = 6;
+    public final static int SLOT_BRONZE_CRAFTING = 1;
+    public final static int SLOT_BRONZE_WEAPON = 2;
+    public final static int SLOT_BRONZE_ARMOUR = 3;
+    public final static int SLOT_BRONZE_TOOL = 4;
+    public final static int SLOT_BRONZE_ACCESSORY = 5;
+    public final static int SLOT_BRONZE_MISC = 6;
 
     // Statistics
     public final static int STATISTIC_XP = 1;
--- app/src/main/java/uk/co/jakelee/blacksmithslots/helper/DatabaseHelper.java
+++ app/src/main/java/uk/co/jakelee/blacksmithslots/helper/DatabaseHelper.java
@@ -12,26 +12,26 @@ import uk.co.jakelee.blacksmithslots.model.Statistic;
 public class DatabaseHelper {
     public static void testSetup() {
         List<Inventory> inventories = new ArrayList<>();
-            inventories.add(new Inventory(Constants.BRONZE_ORE, 100));
+            inventories.add(new Inventory(Constants.RES_BRONZE_ORE, 100));
         Inventory.saveInTx(inventories);
 
         List<Resource> resources = new ArrayList<>();
-            resources.add(new Resource(Constants.BRONZE_ORE));
-            resources.add(new Resource(Constants.BRONZE_BAR));
-            resources.add(new Resource(Constants.BRONZE_DAGGER));
-            resources.add(new Resource(Constants.BRONZE_SWORD));
+            resources.add(new Resource(Constants.RES_BRONZE_ORE));
+            resources.add(new Resource(Constants.RES_BRONZE_BAR));
+            resources.add(new Resource(Constants.RES_BRONZE_DAGGER));
+            resources.add(new Resource(Constants.RES_BRONZE_SWORD));
         Resource.saveInTx(resources);
 
         List<Reward> rewards = new ArrayList<>();
-            rewards.add(new Reward(Constants.BRONZE_CRAFTING, Constants.BRONZE_ORE, 5, 1));
-            rewards.add(new Reward(Constants.BRONZE_CRAFTING, Constants.BRONZE_BAR, 2, 8));
-            rewards.add(new Reward(Constants.BRONZE_CRAFTING, Constants.BRONZE_BAR, 10, 1));
-            rewards.add(new Reward(Constants.BRONZE_CRAFTING, Constants.BRONZE_DAGGER, 1, 1));
-            rewards.add(new Reward(Constants.BRONZE_CRAFTING, Constants.BRONZE_SWORD, 1, 1));
+            rewards.add(new Reward(Constants.SLOT_BRONZE_CRAFTING, Constants.RES_BRONZE_ORE, 5, 1));
+            rewards.add(new Reward(Constants.SLOT_BRONZE_CRAFTING, Constants.RES_BRONZE_BAR, 2, 8));
+            rewards.add(new Reward(Constants.SLOT_BRONZE_CRAFTING, Constants.RES_BRONZE_BAR, 10, 1));
+            rewards.add(new Reward(Constants.SLOT_BRONZE_CRAFTING, Constants.RES_BRONZE_DAGGER, 1, 1));
+            rewards.add(new Reward(Constants.SLOT_BRONZE_CRAFTING, Constants.RES_BRONZE_SWORD, 1, 1));
         Reward.saveInTx(rewards);
 
         List<Slot> slots = new ArrayList<>();
-            slots.add(new Slot(Constants.BRONZE_CRAFTING, 1, Constants.BRONZE_ORE, 2, Enums.Type.Crafting, Enums.Tier.Bronze));
+            slots.add(new Slot(Constants.SLOT_BRONZE_CRAFTING, 1, Constants.RES_BRONZE_ORE, 2, Enums.Type.Crafting, Enums.Tier.Bronze));
         Slot.saveInTx(slots);
 
         List<Statistic> statistics = new ArrayList<>();
